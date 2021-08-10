a6734828b2357ca8245e8106a18a390b98468f02
Alexandr Medvedev <alexano1301@gmail.com>
2021-08-10 16:05:31 +0400

    add validateLoginInput

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,20 +1,54 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { UserInputError } = require("apollo-server");
 
+const {
+  validateRegisterInput,
+  validateLoginInput,
+} = require("../../utils/validators");
 const { SECRET_KEY } = require("../../config");
 const User = require("../../models/User");
 
 module.exports = {
   Mutation: {
+    async login(_, { username, password }) {
+      const { errors, valid } = validateLoginInput(username, password);
+      const user = username.findOne({ username });
+
+      if (!user) {
+        errors.general = "User not found";
+        throw new UserInputError("Wrong credentials", { errors });
+      }
+    },
+
     async register(
       _,
       { registerInput: { username, email, password, confirmPassword } },
       context,
       info
     ) {
-      // TODO: Validate user data
+      // validate user data
+      const { valid, errors } = validateRegisterInput(
+        username,
+        email,
+        password,
+        confirmPassword
+      );
+      if (!valid) {
+        throw new UserInputError("Errors", { errors });
+      }
+
       // TODO: Make sure user doesnt already exist
-      // TODO: hash password and create an auth token
+      const user = User.findOne({ username });
+      if (user) {
+        throw new UserInputError("Username is taken", {
+          errors: {
+            username: "This username is taken",
+          },
+        });
+      }
+
+      // hash password and create an auth token
       password = await bcrypt.hash(password, 12);
 
       const newUser = new User({
@@ -26,7 +60,7 @@ module.exports = {
 
       const res = await newUser.save();
 
-      const token = jwt.string(
+      const token = jwt.sign(
         {
           id: res.id,
           email: res.email,
