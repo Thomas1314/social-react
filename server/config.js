e8bfb928920c68d12af9a155ba355cb4d2b7ef7a
Alexandr Medvedev <alexano1301@gmail.com>
2021-09-13 16:07:26 +0400

    config

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    "mongodb+srv://a-medvedev13:OyyYWlYQtQbcva5v@cluster0.zylxm.mongodb.net/social?retryWrites=true&w=majority",
+  SECRET_KEY: "some very secret key",
+};
